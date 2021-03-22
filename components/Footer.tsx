b831a65d281d4aecf4cf214c93efc1304e8edc39
Anthony Kuang <kuanganthony@gmail.com>
2021-03-22 16:37:53 -0700

    website

---
+++ components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className='pt-[600px] pb-[2000px]'>this is the footer</footer>;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        emerald: colors.emerald,
+        teal: colors.teal,
+        cyan: colors.cyan,
+      },
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
